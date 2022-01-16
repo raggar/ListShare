45d9569827ec626d5485e3354df39d6a65c6000d
Rahul Aggarwal <rahul.aggarwal@faire.com>
2022-01-16 16:15:55 -0500

    added user context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { UserContextProvider } from "./contexts/UserContext";
+
+const Content: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <div>Hello World</div>
+    </header>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>Hello World</div>
-      </header>
-    </div>
+    <UserContextProvider>
+      <Content />
+    </UserContextProvider>
   );
 }
 
