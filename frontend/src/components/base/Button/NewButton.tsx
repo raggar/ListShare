0c69c42871e1e616a61c8b5be7faec8b9f858114
Rahul Aggarwal <rahul.aggarwal@faire.com>
2022-02-16 20:45:39 -0500

    fixed stuff

--- frontend/src/components/base/Button/NewButton.tsx
+++ frontend/src/components/base/Button/NewButton.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import { MdAdd } from "react-icons/md";
 import styles from "../../../styles/styles";
-import { Circle, ReactIcon } from "../";
+import Circle from "../Circle";
+import ReactIcon from "../ReactIcon";
 
 const StyledCircle = styled(Circle).attrs({ size: 48 })`
   z-index: 999;
