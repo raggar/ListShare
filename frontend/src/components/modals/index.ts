771e0f576085d42aa57e5f4b6419dcbcddc2d612
Rahul Aggarwal <rahul.aggarwal@faire.com>
2022-03-06 21:47:48 -0500

    Remove unused code (#22)
    * Edited Readme
    
    * removed unused code

--- frontend/src/components/modals/index.ts
+++ frontend/src/components/modals/index.ts
@@ -2,4 +2,3 @@ export { default as Modal } from "./Modal";
 export { default as AddItemModal } from "./AddItemModal";
 export { default as AddListModal } from "./AddListModal";
 export { default as RegisterModal } from "./RegisterModal";
-export { default as LoginModal } from "./LoginModal";
