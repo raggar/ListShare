4209bd204603435a7735062a6fc0fa8458a3d11a
Rahul Aggarwal <rahul.aggarwal@faire.com>
2022-03-12 14:16:10 -0500

    test

--- frontend/src/components/modals/LoginModal/index.tsx
+++ frontend/src/components/modals/LoginModal/index.tsx
@@ -12,6 +12,7 @@ function LoginModal(props: ModalProps) {
   const [password, setPassword] = useState("");
 
   const handleSubmit = async (e: React.FormEvent<HTMLElement>) => {
+    console.log("Hello");
     e.preventDefault();
     try {
       await loginUser({
