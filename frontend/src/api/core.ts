49c0970b78e0117fd9e61065f0475b25c8347214
Rahul Aggarwal <rahul.aggarwal@faire.com>
2022-03-13 14:17:36 -0400

    changes

--- frontend/src/api/core.ts
+++ frontend/src/api/core.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { QueryClient } from "react-query";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   headers: {
     token: localStorage.getItem("token") ?? "",
   },
